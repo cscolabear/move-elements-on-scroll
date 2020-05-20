1c3551aca559e33a0aa720f3bdbd584b89d62471
Cola <cscolabear@gmail.com>
2020-05-20 15:26:37 +0800

    fix pos

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var lastScrollTop = 0;
 var lastRandomY = 0;
 const yRange = {
-    min: -30,
-    max: 40,
+    min: -20,
+    max: 30,
 };
 const $dog = $('#dog-run');
 const dogWidth = $dog.outerWidth();
@@ -11,27 +11,23 @@ $(function () {
     const getRandomY = (currentTop) => {
         if (currentTop % 2) return lastRandomY; // debounce
 
-        lastRandomY += Math.floor(Math.random() * 10) - 4;
+        lastRandomY += Math.floor(Math.random() * 15) - 10;
         lastRandomY = Math.max(
             yRange.min,
             Math.min(yRange.max, lastRandomY)
         );
         return lastRandomY;
     }
-    $(window).scroll(function () {
+
+    const dogRun = () => {
         const viewHeight = $(window).height();
         const maxHeight = $('body').height();
         const currentTop = $(window).scrollTop() || $('body').scrollTop();
-        let dogPosX = (currentTop / (maxHeight - viewHeight)) * 120; // 現在捲動佔全頁面多入百份比
-        console.table({
-            dogPosX,
-            currentTop,
-            maxHeight,
-            viewHeight
-        });
+        const dogPosX = (currentTop / (maxHeight - viewHeight)) * 100; // 現在捲動佔全頁面多入百份比
+        const dogWidthOffset = currentTop / dogWidth;
 
         const originTransform = $dog.css('transform');
-        let newTransform = `translate3d(calc(-${dogWidth}px + ${dogPosX}vw), ${getRandomY(currentTop)}px , 0)`;
+        let newTransform = `translate3d(${dogPosX + dogWidthOffset}vw, ${getRandomY(currentTop)}px , 0)`;
 
         if (originTransform !== 'none'
         && lastScrollTop > currentTop) {
@@ -42,5 +38,9 @@ $(function () {
             transform: newTransform
         });
         lastScrollTop = currentTop;
+    }
+    $(window).on('scroll resize', function () {
+        dogRun();
     });
+
 });
